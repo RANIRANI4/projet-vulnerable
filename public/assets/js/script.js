8fff4176c3b267e618ec93dce83d770f66918aeb
RANIRANI4 <rani.kabbouri4@gmail.com>
2026-04-08 16:07:32 +0200

    fix upload vulnerability

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,1 +1,23 @@
+
+
 console.log('hello');
+
+let main = document.querySelector('main')
+
+let my_form=document.createElement('form');
+let my_inp=document.createElement('input');
+let my_label=document.createElement('label');
+let my_submit=document.createElement('button');
+
+my_submit.type='submit';
+my_submit.textContent='submit';
+my_form.method='POST';
+
+
+
+main.appendChild(my_form);
+// my_form.appendChild(my_label);
+// my_form.appendChild(my_inp);
+my_form.appendChild(my_submit);
+
+
